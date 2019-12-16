9dc81a1588366394f9a9f148d6772a6d4229114d
David Diez Paz <daviid10paz@gmail.com>
2019-12-16 11:53:28 +0100

    Primer commit - Creacion de dos clases de java.

---
+++ JavaDoc_Practica/src/javafiles/Empleado.java
@@ -0,0 +1,38 @@
+package javafiles;
+
+public class Empleado {
+
+	/**
+	 * Clave Primaria
+	 */
+	
+	public String numempleado;
+	public String nombre;
+	public String dni;
+	
+	
+	public String getNumempleado() {
+		return numempleado;
+	}
+	
+	public void setNumempleado(String numempleado) {
+		this.numempleado = numempleado;
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+	
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	public String getDni() {
+		return dni;
+	}
+	
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+	
+}
---
+++ JavaDoc_Practica/src/javafiles/Jefe.java
@@ -0,0 +1,35 @@
+package javafiles;
+
+public class Jefe {
+
+	public String id;
+	public String nombre;
+	public String dni;
+	
+	
+	public String getId() {
+		return id;
+	}
+	
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+	
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	public String getDni() {
+		return dni;
+	}
+	
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+	
+	
+}
